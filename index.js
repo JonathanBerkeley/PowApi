00a1c4c4584fc9540dfee0c3affd4fc3eddf9713
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-05-04 15:58:14 +0100

    Lots of broken code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-//Following tutorial @ https://www.youtube.com/watch?v=-MTSQjw5DrM
 
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const fs = require('fs');
+var currentID = 0
 
 app.use(express.json())
 
@@ -34,4 +35,80 @@ app.post('/tshirt/:id', (req, res) => {
         });
     }
     
+});
+
+app.get('/logindata', (req, res) => {
+    res.status(200).send({
+        "title": "My google password",
+        "target": "Google",
+        "target_url": "https://google.ie",
+        "password": "abcdefg",
+        "username": "Jonathan"
+    })
+});
+
+app.get('/brute', (req, res) => {
+    res.status(200).sendFile('login_data.json' , { root : __dirname});
+});
+
+app.get('/login/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        res.status(400).send({
+            message: 'No id in request'
+        })
+    }
+    else {
+        res.status(200).send({
+            
+        })
+    }
+    
+});
+
+app.get('/getAcc/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        res.status(401).send({
+            message: 'No ID in request'
+        })
+    }
+    else {
+        try {
+            res.status(200).sendFile(String(id) + ".json" , { root : __dirname});
+        } catch (err) {
+            res.status(401).send({
+                message: 'ID not found'
+            })
+        }
+        
+    }
+});
+
+app.post('/makeAcc', (req, res) => {
+    const { data } = req.body;
+
+    if (!data) {
+        res.status(400).send({
+            message: 'Bad request'
+        })
+    }
+    else {
+        try {
+            let makeJson = JSON.stringify(data, null, 2)
+            fs.writeFile(String(currentID) + ".json", data)
+            ++currentID
+
+            res.status(200).send({
+                message: "Ok"
+            })
+        } catch(err) {
+            console.log(err);
+            res.status(400).send({
+                message: 'Bad request!'
+            })
+        }
+    }
 });
